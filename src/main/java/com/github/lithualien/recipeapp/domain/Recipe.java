53cd00e9827ae2bcfb4c3b41139615ac7dd5673d
Tomas <45800655+lithualien@users.noreply.github.com>
2020-07-21 13:02:43 +0300

    added Ingredient with Recipe relationship (OneToMany and ManyToOne).

--- src/main/java/com/github/lithualien/recipeapp/domain/Recipe.java
+++ src/main/java/com/github/lithualien/recipeapp/domain/Recipe.java
@@ -1,6 +1,8 @@
 package com.github.lithualien.recipeapp.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "recipes")
@@ -26,6 +28,9 @@ public class Recipe {
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients = new HashSet<>();
 
     public Long getId() {
         return id;
@@ -105,5 +110,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
