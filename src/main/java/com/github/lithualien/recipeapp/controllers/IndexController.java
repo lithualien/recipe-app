99069eee895ca05cbd682aa94f0c13fbfcd7a1bf
Tomas <45800655+lithualien@users.noreply.github.com>
2020-07-21 14:27:50 +0300

    JPA Query methods.

--- src/main/java/com/github/lithualien/recipeapp/controllers/IndexController.java
+++ src/main/java/com/github/lithualien/recipeapp/controllers/IndexController.java
@@ -1,14 +1,41 @@
 package com.github.lithualien.recipeapp.controllers;
 
+import com.github.lithualien.recipeapp.domain.Category;
+import com.github.lithualien.recipeapp.domain.UnitOfMeasure;
+import com.github.lithualien.recipeapp.repository.CategoryRepository;
+import com.github.lithualien.recipeapp.repository.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository uomRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository uomRepository) {
+        this.categoryRepository = categoryRepository;
+        this.uomRepository = uomRepository;
+    }
+
     @RequestMapping( {"", "/", "/index"} )
     public String getIndexPage() {
+
+        Optional<Category> categoryOptional =
+                categoryRepository.findByDescription("American");
+
+        Optional<UnitOfMeasure> uomOptional =
+                uomRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat id is: " + categoryOptional.get().getId());
+
+        System.out.println("UOM id is: " + uomOptional.get().getId());
+
         return "index";
     }
+
+
 
 }
