5084bb09ebca48a6d2af624e56561bd3ffb2c388
Tomas <45800655+lithualien@users.noreply.github.com>
2020-08-01 21:30:09 +0300

    Added Lombok to models

--- src/main/java/com/github/lithualien/recipeapp/domain/Category.java
+++ src/main/java/com/github/lithualien/recipeapp/domain/Category.java
@@ -1,9 +1,11 @@
 package com.github.lithualien.recipeapp.domain;
 
+import lombok.Data;
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Data
 @Entity
 @Table(name = "categories")
 public class Category {
@@ -17,27 +19,4 @@ public class Category {
     @ManyToMany(mappedBy = "categories")
     private Set<Recipe> recipes = new HashSet<>();
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Set<Recipe> getRecipes() {
-        return recipes;
-    }
-
-    public void setRecipes(Set<Recipe> recipes) {
-        this.recipes = recipes;
-    }
 }
--- src/main/java/com/github/lithualien/recipeapp/service/RecipeServiceImpl.java
+++ src/main/java/com/github/lithualien/recipeapp/service/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package com.github.lithualien.recipeapp.service;
 
 import com.github.lithualien.recipeapp.domain.Recipe;
 import com.github.lithualien.recipeapp.repository.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -18,6 +20,7 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I am in the service");
         Set<Recipe> recipes = new HashSet<>();
         recipeRepository
                 .findAll()
