46cf2ed0463b01841519d9fe43b8a4f0681627a4
Tomas <45800655+lithualien@users.noreply.github.com>
2020-08-11 14:03:31 +0300

    Test using mockito

---
+++ src/test/java/com/github/lithualien/recipeapp/service/RecipeServiceImplTest.java
@@ -0,0 +1,43 @@
+package com.github.lithualien.recipeapp.service;
+
+import com.github.lithualien.recipeapp.domain.Recipe;
+import com.github.lithualien.recipeapp.repository.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class RecipeServiceImplTest {
+
+    private RecipeServiceImpl recipeService;
+
+    @Mock
+    private RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipes() {
+        Recipe recipe = new Recipe();
+        HashSet recipesData = new HashSet();
+        recipesData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(recipes.size(), 1);
+
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
