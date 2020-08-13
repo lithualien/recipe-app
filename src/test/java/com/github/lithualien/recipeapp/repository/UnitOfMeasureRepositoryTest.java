020e27bcc8094a3b3801f9863dbd6c7b72d63435
Tomas <45800655+lithualien@users.noreply.github.com>
2020-08-13 16:10:28 +0300

    implementation of fail safe plugin.

--- src/test/java/com/github/lithualien/recipeapp/repository/UnitOfMeasureRepositoryTest.java
+++ src/test/java/com/github/lithualien/recipeapp/repository/UnitOfMeasureRepositoryTest.java
@@ -1,12 +1,10 @@
 package com.github.lithualien.recipeapp.repository;
 
 import com.github.lithualien.recipeapp.domain.UnitOfMeasure;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
